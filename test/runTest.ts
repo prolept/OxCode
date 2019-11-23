a8e1456f31f4fac4d60effe175daf2ffe91e0024
m-desktop <7147923+malickf@users.noreply.github.com>
2019-11-23 12:19:18 +0100

    Update runTest.ts

--- test/runTest.ts
+++ test/runTest.ts
@@ -4,7 +4,7 @@ import { runTests } from 'vscode-test';
 
 async function main() {
     try {
-        let testOption = { extensionDevelopmentPath: path.resolve(__dirname, '../'), extensionTestsPath: path.resolve(__dirname, './suite/index') };
+        let testOption = { extensionDevelopmentPath: path.resolve(__dirname, '../../'), extensionTestsPath: path.resolve(__dirname, './suite/index') };
         // Download VS Code, unzip it and run the integration test
         await runTests(testOption);
     } catch (err) {
